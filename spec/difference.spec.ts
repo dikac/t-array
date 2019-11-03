fc49682e9e37dccac9c738dacd994d159f54c922
dikac <rigel234@yahoo.com>
2019-11-04 00:18:29 +0700

    improve difference spec

--- spec/difference.spec.ts
+++ spec/difference.spec.ts
@@ -12,11 +12,53 @@ describe("primitive", function() {
 
 });
 
-// describe("limit", function() {
-//
-//     let array : number[] = [1,2,3,1,5];
-//     let removed = RemovesValue(array, 1, undefined, undefined, undefined, 1);
-//     expect([1]).toEqual(removed);
-//     expect([2,3,1,5]).toEqual(array);
-//
-// });
+describe("custom comparison", function() {
+
+    type Data = {
+        string : String,
+        number : Number,
+        boolean : Boolean,
+    }
+
+    let target : Data[] = [
+        {
+            string : new String('str1'),
+            number : new Number(1),
+            boolean : new Boolean(true),
+        }, {
+            string : new String('str2'),
+            number : new Number(2),
+            boolean : new Boolean(false),
+        }, {
+            string : new String('str3'),
+            number : new Number(3),
+            boolean : new Boolean(true),
+        }
+    ];
+
+    let compare : Data[] = [
+         {
+            string : new String('str2'),
+            number : new Number(2),
+            boolean : new Boolean(false),
+        }
+    ];
+
+    it("without", function() {
+
+        let removed = Difference(target, compare);
+        expect(target).toEqual(removed);
+
+    });
+
+    it("with", function() {
+
+        let removed = Difference(target, compare, (target: Data, comparison: Data) => target.string.toString() === comparison.string.toString());
+        expect(target).not.toEqual(removed);
+        expect(2).toBe(removed.length);
+
+        expect(target[0]).toEqual(removed[0]);
+        expect(target[2]).toEqual(removed[1]);
+
+    });
+});
